aaba7e1a9bdb5f347ce9e50176ad4f77bc83aa0f
sutthiphong-kan <78254992+sutthiphong-kan@users.noreply.github.com>
2021-05-19 22:30:51 +0700

    Description , YAML and Module Installer

--- flask-project/run.bat
+++ flask-project/run.bat
@@ -1,3 +1,12 @@
+pip install flask
+pip install plotly
+pip install requests
+pip install PIL
+pip install base64
+pip install tensorflow
+pip install numpy
+pip install os
+pip install flask-restx
 set flask_app=app
 flask run
 pause > nul
